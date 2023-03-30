04adffae417aa11e952778dc7540465f1aac8bc5
christsven <69843539+christsven@users.noreply.github.com>
2023-03-30 12:13:23 +0200

    Add files via upload

--- sketch_mar31a.ino
+++ sketch_mar31a.ino
@@ -1,34 +1,14 @@
 void blinkFor(int times) {
-  int delayTime = 80;
-  int counter = 0;
-  while (counter < times) {
+  int delayTime = 40;
+  int count = 0;
+  while (count < times) {
     digitalWrite(LED_BUILTIN, HIGH);
     delay(delayTime);
     digitalWrite(LED_BUILTIN, LOW);
     delay(delayTime);
-    counter = counter + 1;
+    count++;
   }
 }
-
-// Output custom text
-void wT(String text) {
-  Serial.print(text);
-  Serial.println();
-}
-
-// Just for end result
-void wN(int number) {
-  Serial.print("Output: ");
-  Serial.print(number);
-  Serial.println();
-}
-
-// Output the actual values
-void i(int i) {
-  //Serial.print(i);
-  //Serial.println();
-}
-
 void setup() {
   Serial.begin(9600);
   // Sets the onboard LED as our output
@@ -36,26 +16,19 @@ void setup() {
   // Gets the HIGH and LOW from Digital 2
   pinMode(2, INPUT);
   // Hello i am alive
-  wT("Hello");
+  Serial.println("Hello");
   blinkFor(3);
 }
-
-
 void loop() {
   int breaks = 0;
   int counter = 0;
   boolean done = false;
-  
   int input = digitalRead(2);
-  i(input);
-
   // Count breaks in a cluster
   while (counter < 20) {
     // Reset
     done = false;
     input = digitalRead(2);
-    i(input);
-
     // Count singular break
     while (input == LOW) {
       if (!done) {
@@ -68,20 +41,18 @@ void loop() {
       }
       // Repeat until circuit gets closed again
       input = digitalRead(2);
-      i(input);
     }
-
     // Count idle time -> either new break or 20x wait
     counter++;
-
     // It does not work without this, i dont know why
     delay(5);
   }
-
   // Output results
   if (breaks != 0) {
-    blinkFor(breaks);
-    wN(breaks);
+    //blinkFor(breaks);
+    Serial.print("Output: ");
+    Serial.print(breaks);
+    Serial.println();
     // Reset
     breaks = 0;
   }
